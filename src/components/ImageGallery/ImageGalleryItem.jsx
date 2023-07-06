cdea21871461c8b185ef00ada9a23c62a8ff0f68
OleksiiVTS <119164270+OleksiiVTS@users.noreply.github.com>
2023-07-06 16:06:52 +0300

    +

--- src/components/ImageGallery/ImageGalleryItem.jsx
+++ src/components/ImageGallery/ImageGalleryItem.jsx
@@ -1,53 +1,95 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import Modal from 'components/Modal/Modal';
 import css from './ImageGallery.module.css';
 
-export default class ImageGalleryItem extends Component {
-  state = {
-    dataImage: {},
-    openModalWindow: false,
-  };
+const ImageGalleryItem = ({ tags, largeImageURL, webformatURL }) => {
+  const [dataImage, setDataImage] = useState({});
+  const [openModalWindow, setOpenModalWindow] = useState(false);
+  const isFirstRender = useRef(true);
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.dataImage !== this.state.dataImage) {
-      this.setState(prevState => ({ openModalWindow: true }));
+  useEffect(() => {
+    if (isFirstRender.current) {
+      isFirstRender.current = false;
+      return;
     }
-  }
 
-  showModalWindow = () => {
-    const { tags, largeImageURL } = this.props;
+    if (dataImage !== {}) {
+      setOpenModalWindow(openModalWindow => !openModalWindow);
+    }
+  }, [dataImage]);
+
+  const showModalWindow = () => {
     const data = { largeImageURL, tags };
-    this.setState(prevState => ({ dataImage: data }));
+    setDataImage(prevDataImage => (prevDataImage = data));
   };
 
-  closeModalWindow = () => {
-    this.setState(prevState => ({ openModalWindow: false }));
+  const closeModalWindow = () => {
+    setOpenModalWindow(openModalWindow => (openModalWindow = false));
   };
 
-  render() {
-    const { tags, webformatURL } = this.props;
-    return (
-      <>
-        <img
-          onClick={this.showModalWindow}
-          className={css.photo}
-          src={webformatURL}
-          alt={tags}
-        />
-        {this.state.openModalWindow === true && (
-          <Modal
-            dataImage={this.state.dataImage}
-            closeModalWindow={this.closeModalWindow}
-          />
-        )}
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <img
+        onClick={showModalWindow}
+        className={css.photo}
+        src={webformatURL}
+        alt={tags}
+      />
+      {openModalWindow && (
+        <Modal dataImage={dataImage} closeModalWindow={closeModalWindow} />
+      )}
+    </>
+  );
+};
+
+// export default class OldImageGalleryItem extends Component {
+//   state = {
+//     dataImage: {},
+//     openModalWindow: false,
+//   };
+
+//   componentDidUpdate(prevProps, prevState) {
+//     if (prevState.dataImage !== this.state.dataImage) {
+//       this.setState(prevState => ({ openModalWindow: true }));
+//     }
+//   }
+
+//   showModalWindow = () => {
+//     const { tags, largeImageURL } = this.props;
+//     const data = { largeImageURL, tags };
+//     this.setState(prevState => ({ dataImage: data }));
+//   };
+
+//   closeModalWindow = () => {
+//     this.setState(prevState => ({ openModalWindow: false }));
+//   };
+
+//   render() {
+//     const { tags, webformatURL } = this.props;
+//     return (
+//       <>
+//         <img
+//           onClick={this.showModalWindow}
+//           className={css.photo}
+//           src={webformatURL}
+//           alt={tags}
+//         />
+//         {this.state.openModalWindow === true && (
+//           <Modal
+//             dataImage={this.state.dataImage}
+//             closeModalWindow={this.closeModalWindow}
+//           />
+//         )}
+//       </>
+//     );
+//   }
+// }
 
 ImageGalleryItem.propTypes = {
   tags: PropTypes.string.isRequired,
   webformatURL: PropTypes.string.isRequired,
   largeImageURL: PropTypes.string.isRequired,
 };
+
+export default ImageGalleryItem;
