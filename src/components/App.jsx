aa3270ac5f72d4eee6e7a4f016167dbc0ea37b76
OleksiiVTS <119164270+OleksiiVTS@users.noreply.github.com>
2023-07-09 20:52:03 +0300

    +

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -27,15 +27,15 @@ export const App = () => {
         );
         return;
       }
-      const normalized = data.hits.map(
-        ({ id, tags, webformatURL, largeImageURL }) => ({
-          id,
-          tags,
-          largeImageURL,
-          webformatURL,
-        })
-      );
-      setDataResult(prevDataResult => [...prevDataResult, ...normalized]);
+      // const normalized = data.hits.map(
+      //   ({ id, tags, webformatURL, largeImageURL }) => ({
+      //     id,
+      //     tags,
+      //     largeImageURL,
+      //     webformatURL,
+      //   })
+      // );
+      setDataResult(prevDataResult => [...prevDataResult, ...data.hits]);
       setTotalImages(data.totalHits);
       setStatus('resolved');
     } catch (error) {
